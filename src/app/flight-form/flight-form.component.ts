cf0fb944f63962bfe73545406a7a9f1f85d5a62b
BeanSpot <joshua.v.kingsley@gmail.com>
2024-02-24 02:48:45 -0500

    api service added, flight form added

---
+++ src/app/flight-form/flight-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './flight-form.component.html',
+  styleUrl: './flight-form.component.css'
+})
+export class FlightFormComponent {
+
+}
